ab6985e8355c2d914c079a815b0f861516a84d0d
violet <violet131519@gmail.com>
2025-11-08 23:22:23 +0800

    test

---
+++ Day3/test.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const ll N = 1e5+5, mod = 998244353, inf = 1e18;
+
+void solve(){
+    
+}
+int main(){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr),std::cout.tie(nullptr);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
